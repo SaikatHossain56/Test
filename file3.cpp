f54580f0d4e4dc725e9d8fca654751339b7e7d92
SaikatHossain56 <saikathossain1035@gmail.com>
2026-03-13 06:18:06 +0600

    initial version

---
+++ file3.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define all(v) v.begin(),v.end()
+
+void solve(){
+    int a, b;  cin>>a>>b;
+cout<<b <<endl;
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+     int t=1 ;
+       cin>>t;
+    while(t--) 
+    solve();
+
+    return 0;
+}
